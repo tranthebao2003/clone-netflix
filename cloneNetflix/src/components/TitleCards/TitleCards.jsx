77e0fa5becc15e444b2d3c40d008e24482249525
= <=>
2024-07-21 16:18:04 +0700

    tìm hiểu về Link ở titlecards và useParams ở player

--- cloneNetflix/src/components/TitleCards/TitleCards.jsx
+++ cloneNetflix/src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
-
+import { Link } from 'react-router-dom'
 
 const TitleCards = ({title, category}) => {
   console.log('title',category)
@@ -76,10 +76,10 @@ const TitleCards = ({title, category}) => {
       <div className="card-list" ref={cardsRef}>
         {apiData.map((card, index) => {
           return (
-            <div className="card" key={index}>
+            <Link className="card" key={index}>
               <img src={`https://image.tmdb.org/t/p/w500/`+card.backdrop_path} alt="" />
               <p>{card.original_title}</p>
-            </div>
+            </Link>
           );
         })}
       </div>
--- cloneNetflix/src/pages/Player/Player.jsx
+++ cloneNetflix/src/pages/Player/Player.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
 import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useParams } from 'react-router-dom'
 
 const Player = () => {
+
+  const {id} = useParams()
+
+  const [apiData, setApiData] = useState({
+    name:"",
+    key:"",
+    published_at:"",
+    type: ""
+  })
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzdkNWVlOGYxMTQ5NTU3MGNjMmRlNDJhNTg5YTIwNiIsIm5iZiI6MTcyMTUzMDM4Ni4xMzU1OCwic3ViIjoiNjY5Yzc1ODQ0N2ViMDY2NmFjY2Q3MWVjIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.lJbvJ89AoY7EtMJjnMTdbtRs721B_uxIeD5fN99KyxQ'
+    }
+  };
+  
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results[0]))
+    .catch(err => console.error(err));
+  }, [])
+
   return (
     <div className="player">
       <img src={back_arrow_icon} alt="" />
@@ -13,15 +38,15 @@ const Player = () => {
       <iframe
         width="90%"
         height="90%"
-        src="https://www.youtube.com/embed/hkHHwA-vEyQ"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
         title="trailer"
         frameBorder="0"
         allowFullScreen
       ></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0,10).split('-').reverse().join('-')}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   );
